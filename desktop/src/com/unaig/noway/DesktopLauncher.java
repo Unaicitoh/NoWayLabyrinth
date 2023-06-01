c401d1bd4fa337a3eaeee0e702b9142907502ef3
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2023-06-01 10:33:19 +0200

    importing initial libgdx setup

---
+++ desktop/src/com/unaig/noway/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.unaig.noway;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.unaig.noway.NoWayLabyrinth;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("No Way Labyrinth");
+		new Lwjgl3Application(new NoWayLabyrinth(), config);
+	}
+}
