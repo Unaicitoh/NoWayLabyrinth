207542ad8e107be6eb79cc0d08f897b8a41bbeb0
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2023-06-17 04:47:03 +0200

    very minor little refactor

--- core/src/com/unaig/noway/entities/enemies/SpiderEnemy.java
+++ core/src/com/unaig/noway/entities/enemies/SpiderEnemy.java
@@ -21,7 +21,7 @@ import static com.unaig.noway.util.Direction.*;
 
 public class SpiderEnemy extends Enemy {
 
-    private static final Pool<SpiderEnemy> spiderPool = new Pool<>() {
+    private static final Pool<SpiderEnemy> spiderPool = new Pool<SpiderEnemy>() {
         @Override
         protected SpiderEnemy newObject() {
             return new SpiderEnemy();
